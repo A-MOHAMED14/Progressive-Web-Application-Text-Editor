293fe2e99ff3ad5ee1a38faebd8e06953390dfe8
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-30 11:45:37 +0100

    Add style-loader and css-loader configuration for handling CSS files in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,12 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
     },
   };
 };
